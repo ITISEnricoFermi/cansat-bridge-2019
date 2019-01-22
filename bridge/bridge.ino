9b64d61dacb014e83fa6381c84c1614938c10219
Ernesto Montada Romero <e.montada0@gmail.com>
2019-01-22 17:18:31 +0100

    Stampa dati json

--- bridge/bridge.ino
+++ bridge/bridge.ino
@@ -3,36 +3,31 @@
 #include <LoRa.h>
 //#include <ArduinoJson.h>
 
-
 void setup()
 {
     Serial.begin(115200);
-    Wire.begin(4);
-    Wire.onReceive(receiveEvent);
     while(!Serial){}
+
+    Wire.begin(4);
     
-   if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  } 
+    Wire.onReceive(receiveEvent);
+      
+    if (!LoRa.begin(433E6)) {
+      Serial.println("Starting LoRa failed!");
+      while (1);
+    }
+
+    Serial.println("Setup complete!");
 }
 void loop()
 {
   delay(100);
-
-  
 }
 
 void receiveEvent(int howMany) {
   while (1 < Wire.available()) { 
     char c = Wire.read(); 
-    //Serial.print(c);         
-  }
-  int x = Wire.read();    
-  Serial.println(x);        
-  
-  LoRa.beginPacket();
-  LoRa.print("temp: ");
-  LoRa.print(x);
-  LoRa.endPacket();
+    Serial.print(c);
+  }        
+  Serial.println();
 }
