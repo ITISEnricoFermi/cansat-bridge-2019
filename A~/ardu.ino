e962a9e1c5ba9db16b3c6fe0935fe0f40a85f6f7
Alby7503 <alberto.vona24@gmail.com>
2019-01-29 09:26:31 +0100

    Log completo
    Aggiunto codice di debug (da rimuovere con prove completate)

--- A~/ardu.ino
+++ A~/ardu.ino
@@ -12,16 +12,16 @@ union u_tag {
 union w_tag {
   byte b[sizeof(double)];
   double dval;
-}w;
+} w;
 
 double readDouble()
 {
-   short i = 0;
-    while (Wire.available() && i < sizeof(double))
-    {
-      w.b[i++] = Wire.read();
-    }
-    return w.dval;
+  short i = 0;
+  while (Wire.available() && i < sizeof(double))
+  {
+    w.b[i++] = Wire.read();
+  }
+  return w.dval;
 }
 #pragma endregion
 
@@ -31,237 +31,338 @@ const int chipSelect = 4; //?
 File dataFile = SD.open("log.txt", FILE_WRITE);
 
 float readFloat()
+{
+  short i = 0;
+  while (Wire.available() && i < sizeof(float))
   {
-    short i = 0;
-    while (Wire.available() && i < sizeof(float))
-    {
-      u.b[i++] = Wire.read();
-    }
-    return u.fval;
+    u.b[i++] = Wire.read();
   }
+  return u.fval;
+}
 
-  void logFile(float var, int type) //type 1:temp 2:press 3:humi 4:altezza
+void logFile(float var, int type)
+{
+  if (dataFile)
   {
-    if (dataFile)
+    switch (type)
     {
-      switch (type)
-      {
-      case 1:
-        dataFile.print("Temperatura: ");
-        dataFile.print(var);
-        dataFile.println();
-        break;
+    case 1:
+      dataFile.print("Temperatura: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
 
-        case 2:
-        dataFile.print("Pressione: ");
-        dataFile.print(var);
-        dataFile.println();
-        break;
+    case 2:
+      dataFile.print("Pressione: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
 
-        case 3:
-        dataFile.print("Umidità: ");
-        dataFile.print(var);
-        dataFile.println();
-        break;
+    case 3:
+      dataFile.print("Umidità: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
 
-        case 4:
-        dataFile.print("Altezza: ");
-        dataFile.print(var);
-        dataFile.println();
-        break;
+    case 4:
+      dataFile.print("Altezza: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
 
-      default:
-        break;
-      }
+    case 5:
+      dataFile.print("Latitudine: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 6:
+      dataFile.print("Longitudine: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 7:
+      dataFile.print("Accelerazione X: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 8:
+      dataFile.print("Accelerazione Y: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 9:
+      dataFile.print("Accelerazione Z: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 10:
+      dataFile.print("Gravità X: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 11:
+      dataFile.print("Gravità Y: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 12:
+      dataFile.print("Gravità Z: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 13:
+      dataFile.print("Magnetismo X: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 14:
+      dataFile.print("Magnetismo Y: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    case 15:
+      dataFile.print("Magnetismo Z: ");
+      dataFile.print(var);
+      dataFile.println();
+      break;
+
+    default:
+      break;
     }
   }
+}
 
 void receiveEvent(int howMany)
+{
+  while (0 < Wire.available())
   {
-    while (0 < Wire.available())
+    char c = Wire.read();
+    LoRa.beginPacket();
+    bool ended = false;
+    switch (c)
     {
-      char c = Wire.read();
-      LoRa.beginPacket();
-      bool ended = false;
-      switch (c)
-      {
-      case 'a':
-      {
-        float localTemp = readFloat();
-        LoRa.print("Temperatura: ");
-        LoRa.print(localTemp);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(localTemp, 1);
-        ended = true;
-      }
+    case 'a':
+    {
+      float localTemp = readFloat();
+      LoRa.print("Temperatura: ");
+      LoRa.print(localTemp);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(localTemp, 1);
+      ended = true;
+      Serial.print("Temperatura: ");
+      Serial.print(localTemp); //debug
+    }
 
-      case 'b':
-      {
-        
-        float localHumi = readFloat();
-        LoRa.print("Umidita: ");
-        LoRa.print(localHumi);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(localHumi, 3);
-        ended = true;
-      }
+    case 'b':
+    {
 
-      case 'c':
-      {
-        float localPress = readFloat();
-        LoRa.print("Pressione: ");
-        LoRa.print(localPress);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(localPress, 2);
-        ended = true;
-      }
+      float localHumi = readFloat();
+      LoRa.print("Umidita: ");
+      LoRa.print(localHumi);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(localHumi, 3);
+      ended = true;
+      Serial.print("Umidita: ");
+      Serial.print(localHumi); //debug
+    }
 
-      case 'd':
-      {
-        double lat = readDouble();
-        LoRa.print("Latitudine: ");
-        LoRa.print(lat);
-        LoRa.endPacket();
-        logFile(lat, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'c':
+    {
+      float localPress = readFloat();
+      LoRa.print("Pressione: ");
+      LoRa.print(localPress);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(localPress, 2);
+      ended = true;
+      Serial.print("Pressione: ");
+      Serial.print(localPress); //debug
+    }
 
-      case 'e':
-      {
-        double lon = readDouble();
-        LoRa.print("Latitudine: ");
-        LoRa.print(lat);
-        LoRa.endPacket();
-        logFile(lon, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'd':
+    {
+      double lat = readDouble();
+      LoRa.print("Latitudine: ");
+      LoRa.print(lat);
+      LoRa.endPacket();
+      logFile(lat, 5);
+      ended = true;
+      Serial.print("Latitudine: ");
+      Serial.print(lat); //debug
+    }
 
-      case 'f':
-      {
-        float altezza = readFloat();
-        LoRa.print("Altezza: ");
-        LoRa.print(altezza);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(altezza, 4);
-        ended = true;
-      }
+    case 'e':
+    {
+      double lon = readDouble();
+      LoRa.print("Longitudine: ");
+      LoRa.print(lon);
+      LoRa.endPacket();
+      logFile(lon, 6);
+      ended = true;
+      Serial.print("Longitudine: ");
+      Serial.print(lon); //debug
+    }
 
-      case 'g':
-      {
-        float ax = readFloat();
-        LoRa.print("Accelerazione X: ");
-        LoRa.print(ax);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(ax, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'f':
+    {
+      float altezza = readFloat();
+      LoRa.print("Altezza: ");
+      LoRa.print(altezza);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(altezza, 4);
+      ended = true;
+      Serial.print("Altezza: ");
+      Serial.print(altezza); //debug
+    }
 
-      case 'h':
-      {
-        float ay = readFloat();
-        LoRa.print("Accelerazione Y: ");
-        LoRa.print(ay);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(ay, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'g':
+    {
+      float ax = readFloat();
+      LoRa.print("Accelerazione X: ");
+      LoRa.print(ax);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(ax, 7);
+      ended = true;
+      Serial.print("Accelerazione X: ");
+      Serial.print(ax); //debug
+    }
 
-      case 'i':
-      {
-        float az = readFloat();
-        LoRa.print("Accelerazione Z: ");
-        LoRa.print(az);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(az, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'h':
+    {
+      float ay = readFloat();
+      LoRa.print("Accelerazione Y: ");
+      LoRa.print(ay);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(ay, 8);
+      ended = true;
+      Serial.print("Accelerazione Y: ");
+      Serial.print(ay); //debug
+    }
 
-      case 'l':
-      {
-        float gx = readFloat();
-        LoRa.print("Gravità X: ");
-        LoRa.print(gx);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(gx, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'i':
+    {
+      float az = readFloat();
+      LoRa.print("Accelerazione Z: ");
+      LoRa.print(az);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(az, 9);
+      ended = true;
+      Serial.print("Accelerazione Z: ");
+      Serial.print(az); //debug
+    }
 
-      case 'm':
-      {
-        float gy = readFloat();
-        LoRa.print("Gravità Y: ");
-        LoRa.print(gx);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(gy, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'l':
+    {
+      float gx = readFloat();
+      LoRa.print("Gravità X: ");
+      LoRa.print(gx);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(gx, 10);
+      ended = true;
+      Serial.print("Gravità X: ");
+      Serial.print(gx); //debug
+    }
 
-      case 'n':
-      {
-        float gz = readFloat();
-        LoRa.print("Gravità Z: ");
-        LoRa.print(gz);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(gz, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'm':
+    {
+      float gy = readFloat();
+      LoRa.print("Gravità Y: ");
+      LoRa.print(gy);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(gy, 11);
+      ended = true;
+      Serial.print("Gravità Y: ");
+      Serial.print(gy); //debug
+    }
 
-      case 'o':
-      {
-        float mx = readFloat();
-        LoRa.print("Magnetismo X: ");
-        LoRa.print(mx);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(mx, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'n':
+    {
+      float gz = readFloat();
+      LoRa.print("Gravità Z: ");
+      LoRa.print(gz);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(gz, 12);
+      ended = true;
+      Serial.print("Gravità Z: ");
+      Serial.print(gz); //debug
+    }
 
-      case 'p':
-      {
-        float my = readFloat();
-        LoRa.print("Magnetismo Y: ");
-        LoRa.print(my);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(my, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'o':
+    {
+      float mx = readFloat();
+      LoRa.print("Magnetismo X: ");
+      LoRa.print(mx);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(mx, 13);
+      ended = true;
+      Serial.print("Magnetismo X: ");
+      Serial.print(mx); //debug
+    }
 
-      case 'q':
-      {
-        float mz = readFloat();
-        LoRa.print("Magnetismo Z: ");
-        LoRa.print(mz);
-        LoRa.println();
-        LoRa.endPacket();
-        logFile(mz, ); //metodo logFile serve type
-        ended = true;
-      }
+    case 'p':
+    {
+      float my = readFloat();
+      LoRa.print("Magnetismo Y: ");
+      LoRa.print(my);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(my, 14);
+      ended = true;
+      Serial.print("Magnetismo Y: ");
+      Serial.print(my); //debug
+    }
 
-      default:
-        break;
-      }
-      if (!ended)
-      {
-        LoRa.endPacket();
-      }
+    case 'q':
+    {
+      float mz = readFloat();
+      LoRa.print("Magnetismo Z: ");
+      LoRa.print(mz);
+      LoRa.println();
+      LoRa.endPacket();
+      logFile(mz, 15);
+      ended = true;
+      Serial.print("Magnetismo Z: ");
+      Serial.print(mz); //debug
+    }
+
+    default:
+      break;
+    }
+    if (!ended)
+    {
+      LoRa.endPacket();
+    }
+    else
+    {
       ended = false;
     }
+    Serial.println();
   }
+}
 
 void setup()
 {
+  Serial.begin(9600); //debug
   Wire.begin(4);
   if (!LoRa.begin(433E6))
   {
@@ -276,8 +377,9 @@ void setup()
       ;
     Wire.onReceive(receiveEvent);
   }
+  Serial.println("Setup Completato"); //debug
 }
-  void loop()
-  {
-    delay(100);
-  }
+void loop()
+{
+  delay(100);
+}
